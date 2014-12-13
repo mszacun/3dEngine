0250d788a588e5fd39b035757ee660ec03111011
Marcin Szachun <mszachun@gmail.com>
2014-12-13 12:01:58 +0100

    Configure QT library
    Benchmark fillinf Pixmap with pixels

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <QApplication>
+#include <QPushButton>
+#include <QPixmap>
+#include <QPainter>
+#include <sys/time.h>
+#include <functional>
 
-int main()
+const int HEIGHT = 1080;
+const int WIDTH = 1080;
+void BenchmarkQPixmap();
+
+long long Benchmark(const std::function<void()>& f)
 {
-    std::cout << "Test" << std::endl;
-    return 0;
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    f();
+
+    gettimeofday(&end, NULL);
+    return (end.tv_sec * (unsigned int)1e6 +   end.tv_usec) -
+         (start.tv_sec * (unsigned int)1e6 + start.tv_usec);
+}
+
+
+int main(int argc, char** argv)
+{
+
+        QApplication app (argc, argv);
+
+        for (int i = 0; i < 10; i++)
+            std::cout << Benchmark(BenchmarkQPixmap) << std::endl;
+        QPushButton button ("Hello world !");
+        button.show();
+
+
+        return app.exec();
+}
+
+void BenchmarkQPixmap()
+{
+    QPixmap pixmap(WIDTH, HEIGHT);
+    QPainter painter(&pixmap);
+
+    for (int x = 0; x < WIDTH; x++)
+        for (int y = 0; y < HEIGHT; y++)
+            painter.drawPoint(x,y);
 }
