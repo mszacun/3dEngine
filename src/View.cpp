dd852a76e28ae170887838929aa24092ed0481a8
Marcin Szachun <mszachun@gmail.com>
2015-01-10 11:48:15 +0100

    Split View into 2 widgets for orthagonal and perspective camera
    This will allow to implement moving camera with mouse

--- src/View.cpp
+++ src/View.cpp
@@ -2,9 +2,43 @@
 #include "Controler.h"
 #include <chrono>
 
+void OrthagonalViewport::DrawScene(QImage& scene, const QPoint& perspectiveCameraPosition)
+{
+    buffer_ = scene;
+    perspectiveCameraPosition_ = perspectiveCameraPosition;
+    update();
+}
+
+void OrthagonalViewport::paintEvent(QPaintEvent* event)
+{
+    QPainter painter(this);
+
+    painter.drawImage(0, 0, buffer_);
+}
+
+void PerspectiveViewport::DrawScene(QImage& scene)
+{
+    buffer_ = scene;
+    update();
+}
+
+void PerspectiveViewport::paintEvent(QPaintEvent* event)
+{
+    QPainter painter(this);
+
+    painter.drawImage(0, 0, buffer_);
+}
+
 View::View()
 {
     setFocusPolicy(Qt::ClickFocus);
+    
+    layout.addWidget(&perspectiveView_, 0, 0);
+    layout.addWidget(&frontView_, 0, 1);
+    layout.addWidget(&sideView_, 1, 1);
+    layout.addWidget(&topView_, 1, 0);
+
+    setLayout(&layout);
 }
 
 void View::SetScene(const Scene2D& scene)
@@ -25,24 +59,25 @@ QSize View::sizeHint() const
 void View::SetControler(ControlerPtr controler)
 {
     controler_ = controler;
+    UpdateCameraViews();
 }
 
-void View::paintEvent(QPaintEvent* event)
+void View::UpdateCameraViews()
 {
     auto start = std::chrono::steady_clock::now();
-    QPainter painter(this);
+//    QPainter painter(this);
 
     QImage i = controler_->GetRenderedPerspectiveView();
-    painter.drawImage(0, 0, i);
+    perspectiveView_.DrawScene(i);
 
     i = controler_->GetFrontView();
-    painter.drawImage(500, 500, i);
+    frontView_.DrawScene(i, QPoint(0, 0));
 
     i = controler_->GetSideView();
-    painter.drawImage(0, 500, i);
+    sideView_.DrawScene(i, QPoint(0, 0));
 
     i = controler_->GetTopView();
-    painter.drawImage(500, 0, i);
+    topView_.DrawScene(i, QPoint(0, 0));
 
     auto end = std::chrono::steady_clock::now();
 
@@ -55,6 +90,6 @@ void View::paintEvent(QPaintEvent* event)
 void View::keyPressEvent(QKeyEvent* event)
 {
     controler_->KeyPressed(event->key());
-    update();
+    UpdateCameraViews();
     QWidget::keyPressEvent(event);
 }
--- src/View.h
+++ src/View.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <QPainter>
 #include <QKeyEvent>
+#include <QGridLayout>
 #include <memory>
 #include <cassert>
 
@@ -11,6 +12,36 @@
 
 class Controler;
 typedef std::shared_ptr<Controler> ControlerPtr;
+
+class OrthagonalViewport : public QWidget
+{
+    Q_OBJECT
+
+    public:
+        void DrawScene(QImage& scene, const QPoint& perspectiveCameraPosition);
+
+    protected:
+        void paintEvent(QPaintEvent* event) override;
+
+    private:
+        QPoint perspectiveCameraPosition_;
+        QImage buffer_;
+};
+
+class PerspectiveViewport : public QWidget
+{
+    Q_OBJECT
+
+    public:
+        void DrawScene(QImage& scene);
+
+    protected:
+        void paintEvent(QPaintEvent* event) override;
+
+    private:
+        QPoint perspectiveCameraPosition_;
+        QImage buffer_;
+};
 
 class View : public QWidget
 {
@@ -26,12 +57,18 @@ class View : public QWidget
         void SetControler(ControlerPtr controler);
 
     protected:
-        void paintEvent(QPaintEvent* event) override;
+        void UpdateCameraViews();
         void keyPressEvent(QKeyEvent* event) override;
 
     private:
-        Scene2D scene_;
+        PerspectiveViewport perspectiveView_;
+        OrthagonalViewport sideView_;
+        OrthagonalViewport topView_;
+        OrthagonalViewport frontView_;
+        QGridLayout layout;
+
         ControlerPtr controler_;
+        Scene2D scene_;
 
 };
 
